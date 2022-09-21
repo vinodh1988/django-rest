9b75e5dcc6806a8406fcb11ccad2a040b05bb218
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-21 11:52:22 +0530

    apiview

---
+++ project5/firstrest/serializers/__init__.py
@@ -0,0 +1,1 @@
+from serializers.serializers import PersonSerializer
--- project5/firstrest/views.py
+++ project5/firstrest/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from firstrest.models import Person
 from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from serializers import PersonSerializer
+
 # Create your views here.
 def getPeople(request):
     result=Person.objects.all()
     return JsonResponse({'people': list(result.values())})
+
+@api_view(['GET'])
+def peopleList(request):
+    result=Person.objects.all()
+    serializer=PersonSerializer(result)
+    return Response(serializer.data)
