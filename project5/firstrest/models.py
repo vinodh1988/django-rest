3082bedf5069ab82d688407e6a473ffb06a4194a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-22 10:04:27 +0530

    new-model

--- project5/firstrest/models.py
+++ project5/firstrest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import CheckConstraint
 
 # Create your models here.
 
@@ -9,3 +10,25 @@ class Person(models.Model):
 
     def __str__(self):
         return  str({'sno':self.sno,'name':self.name,'city':self.city})
+
+class Author(models.Model):
+    authorid=models.IntegerField(PrimaryKey=True)
+    name=models.CharField(max_length=30,null=False)
+    country=models.CharField(max_length=30,null=False)
+
+    def __str__(self):
+        return str({'authorid':self.authorid,'name':self.name,'country':self.country})
+
+class Book(models.Model):
+    bookid=models.IntegerField(PrimaryKey=True)
+    name=models.CharField(max_length=30,null=False)
+    price=models.FloatField(null=False)
+    category=models.CharField(max_length=30,null=False)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,related_name="books")
+
+    def __str__(self):
+        return str({'bookid':self.bookid,'name':self.name,'price':self.price,
+        'category':self.category})
+
+ 
+    
