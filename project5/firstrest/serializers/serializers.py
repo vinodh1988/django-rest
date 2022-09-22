a77c0a7dd44dcac30b0c747bc14a1c74d016c3d8
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-22 11:34:01 +0530

    relationship

--- project5/firstrest/serializers/serializers.py
+++ project5/firstrest/serializers/serializers.py
@@ -17,4 +17,4 @@ class AuthorSerializer(serializers.ModelSerializer):
     books=BookSerializer(many=True,read_only=True)
     class Meta:
         model= Author
-        fields = '__all__'
+        fields = ('authorid','name','country','books')
