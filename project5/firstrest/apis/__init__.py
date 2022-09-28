1c72ebd7c521f038e58d5e64271f93faf37ec40c
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-28 15:17:20 +0530

    genericview

--- project5/firstrest/apis/__init__.py
+++ project5/firstrest/apis/__init__.py
@@ -1,1 +1,2 @@
 from firstrest.apis.bookapi import AuthorAPI, BookAPI
+from firstrest.apis.genapi import AuthorGAPI
--- project5/firstrest/urls.py
+++ project5/firstrest/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from firstrest.views import getPeople, peopleList, view_home,schema_view
 from firstrest.apis import AuthorAPI,BookAPI
-
+from firstrest.apis import AuthorGAPI
 urlpatterns=[
     path('list/',getPeople,name='people-list'),
     path('peoplelist/',peopleList),
     path('authors/',AuthorAPI.as_view(),name='author-list'),
+    path('gauthors/',AuthorGAPI.as_view(),name='author-generic'),
     path('authors/<int:pk>',AuthorAPI.as_view(),name='author-by-id'),
     path('books/',BookAPI.as_view(),name='book-list'),
     path('resthome/',view_home, name="view home page"),
